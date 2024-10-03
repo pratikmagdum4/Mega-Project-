3bc09bea68e25fcd29cb8b82e0a59a958e6bf45d
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-10-03 14:34:58 +0530

    fix:app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/home';
-import Signup from './pages/signup/signup'; // Assuming signup page is in signup folder
-import Signup2 from './pages/signup/signup';
-import Login from './pages/login/login';
+import Home from './pages/home/home.jsx';
+import Signup from './pages/SignUp/signup.jsx';
+import Login from './pages/login/login.jsx';
 
 function App() {
   return (
@@ -14,10 +11,11 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup2 />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </Router>
   );
 }
+
 
 export default App;
