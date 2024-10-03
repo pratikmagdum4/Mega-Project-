b895138102be52def9d6a6d066f1a83781b22a0e
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-10-03 18:11:15 +0530

    fix:minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home/home'
-import Signup from './pages/signup/signup'  // Assuming signup page is in signup folder
-import Signup2 from './pages/signup/signup'
-import Login from './pages/login/login'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home';
+import Signup from './pages/signup/signup'; // Assuming signup page is in signup folder
+import Signup2 from './pages/signup/signup';
+import Login from './pages/login/login';
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup2 />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup2 />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
