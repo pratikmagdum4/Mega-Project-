c004ba4f3976f286a7cde8327913399742d7ee56
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-10-03 14:41:59 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home.jsx';
-import Signup from './pages/SignUp/signup.jsx';
+import Signup from './pages/SignUp/Signup.jsx';
 import Login from './pages/login/login.jsx';
 
 function App() {
